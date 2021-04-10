ee5c5e3b789857a6d02a5e3e9113f148b95902a4
ederjunio <eder.xavier.ti@gmail.com>
2021-04-10 12:06:01 -0300

    commit on main

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {WebView} from 'react-native-webview';
+
+
+const Tab = createBottomTabNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Aba Texto" component={Tela01}></Tab.Screen>
+        <Tab.Screen name="Aba YouTube" component={Tela02}></Tab.Screen>
+        <Tab.Screen name="Aba somente vídeo" component={Tela03}></Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -17,5 +26,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 'top'
   },
 });
+
+function Tela01(){
+  return(
+      <WebView style={{flex:1, marginTop:40}} source={{uri: 'https://www.academia.org.br/academicos/manuel-bandeira/biografia'}}></WebView>
+  );
+}
+
+function Tela02(){
+  return(
+    <WebView style={{flex:1, height: 300}} source={{ uri: 'https://www.youtube.com/embed/GV0B6nGf9mcrel=0&autoplay=0&showinfo=0&controls=1&fullscreen=1' }}/>
+  );
+}
+
+function Tela03(){
+  return(
+      <WebView style={{flex:1, marginTop:40}} javaScriptEnabled={true}
+          source={{html:"<html><body><iframe marginTop='40' width='1000' height='700' src='https://www.youtube.com/embed/rHANBi7E2cI' frameborder='0' allowfullscreen> </iframe></body></html>"}}
+      ></WebView>
+  );
+}
